076618b5c5dbd103cbaa6890c413ab7e5d6467c6
Hari <619202904@qq.com>
2020-05-18 15:32:55 +0800

    update

--- src/Calculator/index.ts
+++ src/Calculator/index.ts
@@ -1,77 +1,117 @@
 export class Calculator {
-  private val1Arr: [string, string | null]
+  private num1Arr: [string, string?]
 
-  constructor (val1: string) {
-    this.val1Arr = val1.split('.')
+  constructor (num1: string) {
+    this.num1Arr = this.removeDecimalExtraZero(num1).split('.')
+  }
+
+  /** 去除小数位多余的0 */
+  private removeDecimalExtraZero (val: string) {
+    return Number.parseFloat(val).toString()
   }
 
   /** 相乘 */
-  multiply (val2: string) {
-    let val1FloatResultArr = []
-    let val1IntRemainder = 0
+  multiply (num2: string) {
+    const num2Arr = this.removeDecimalExtraZero(num2).split('.') as [string, string?]
+    // 保存小数位长度
+    const num2DecimalLength = num2Arr[1]?.length
+    // 把num2变为整数
+    const num2Int = Number.parseInt(num2Arr.join(''))
 
-    const val2Arr = val2.split('.')
-    let val2Num = Number.parseInt(val2)
-    let val2FloatLen = 0
+    // 需要与下一位数相加的值
+    let nextSum = 0
 
-    if (val2Arr[1]) {
-      //         记录小数位长度
-      val2FloatLen = val2Arr[1].length
+    // 当第一个因数有小数位时
+    if (this.num1Arr[1]) {
+      // 把小数位转换位数组
+      const num1ArrDecimalArr = this.num1Arr[1].split('')
 
-      val2Num = Number.parseInt(val2Arr.join(''))
-    }
+      for (let i = num1ArrDecimalArr.length - 1, sumArr = '', nextSumStr = ''; i >= 0; i--) {
+        // 从最后一位数开始计算 * num2的整数
+        sumArr = (Number.parseInt(num1ArrDecimalArr[i]) * num2Int).toString()
 
-    if (this.val1Arr[1]) {
-      const productArr = this.val1Arr[1]
-        .split('')
-        .reverse()
-        .map(ele => Number.parseInt(ele) * val2Num)
+        num1ArrDecimalArr[i] = (Number.parseInt(sumArr.substr(sumArr.length - 1, 1)) + nextSum).toString()
 
-      if (productArr.length > 1) {
-        for (let i = 0; i < productArr.length - 1; i++) {
-          productArr[i] = productArr[i]
-            .toString()
-            .replace(/(\d*)(\d{1})$/, (_v1, v2, v3) => {
-              if (v2) {
-                (productArr[i + 1]) += Number.parseInt(v2)
-              }
+        nextSumStr = sumArr.substr(0, sumArr.length - 1)
 
-              return v3
-            })
-        }
+        nextSum = nextSumStr ? Number.parseInt(nextSumStr) : 0
       }
 
-      val1FloatResultArr = productArr.reverse()
-
-      val1FloatResultArr[0] = val1FloatResultArr[0]
-        .toString()
-        .replace(/(\d*)(\d{1})$/, (_v1, v2, v3) => {
-          if (v2) {
-            val1IntRemainder = Number.parseInt(v2)
-          }
-
-          return v3
-        })
+      this.num1Arr[1] = num1ArrDecimalArr.join('')
     }
 
-    this.val1Arr[0] = (
-      Number.parseInt(val1Arr[0]) * val2Num +
-      val1IntRemainder
-    ).toString()
+    // 计算整数部分
+    this.num1Arr[0] = (Number.parseInt(this.num1Arr[0]) * num2Int).toString()
 
-    this.val1Arr[1] = val1FloatResultArr.join('')
+    if (num2DecimalLength && num2DecimalLength > 0) {
+      // this.num1Arr[1] = this.num1Arr[0].substr(this.num1Arr[0].length - num2DecimalLength, num2DecimalLength) + this.num1Arr[1]
 
-    this.val1Arr[0] = this.val1Arr[0].padStart(val2FloatLen, '0')
-
-    const regexp = new RegExp(`(\\d*)(\\d{${val2FloatLen}})$`)
-
-    this.val1 = val1Arr[0].replace(regexp, (v1, v2, v3) => {
-      const float = v3 + val1Arr[1]
-
-      return float ? `${v2 || '0'}.${float}` : v2
-    })
+      this.num1Arr[0] = this.num1Arr[0].substr(0, this.num1Arr[0].length - num2DecimalLength) || '0'
+    }
 
     return this
+
+    // let val2Num = Number.parseInt(num2)
+    // let val2FloatLen = 0
+
+    // if (num2Arr[1]) {
+    //   //         记录小数位长度
+    //   val2FloatLen = num2Arr[1].length
+
+    //   val2Num = Number.parseInt(num2Arr.join(''))
+    // }
+
+    // if (this.num1Arr[1]) {
+    //   const productArr = this.num1Arr[1]
+    //     .split('')
+    //     .reverse()
+    //     .map(ele => Number.parseInt(ele) * val2Num)
+
+    //   if (productArr.length > 1) {
+    //     for (let i = 0; i < productArr.length - 1; i++) {
+    //       productArr[i] = productArr[i]
+    //         .toString()
+    //         .replace(/(\d*)(\d{1})$/, (_v1, v2, v3) => {
+    //           if (v2) {
+    //             (productArr[i + 1]) += Number.parseInt(v2)
+    //           }
+
+    //           return v3
+    //         })
+    //     }
+    //   }
+
+    //   val1FloatResultArr = productArr.reverse()
+
+    //   val1FloatResultArr[0] = val1FloatResultArr[0]
+    //     .toString()
+    //     .replace(/(\d*)(\d{1})$/, (_v1, v2, v3) => {
+    //       if (v2) {
+    //         val1IntRemainder = Number.parseInt(v2)
+    //       }
+
+    //       return v3
+    //     })
+    // }
+
+    // this.num1Arr[0] = (
+    //   Number.parseInt(val1Arr[0]) * val2Num +
+    //   val1IntRemainder
+    // ).toString()
+
+    // this.num1Arr[1] = val1FloatResultArr.join('')
+
+    // this.num1Arr[0] = this.num1Arr[0].padStart(val2FloatLen, '0')
+
+    // const regexp = new RegExp(`(\\d*)(\\d{${val2FloatLen}})$`)
+
+    // this.val1 = val1Arr[0].replace(regexp, (v1, v2, v3) => {
+    //   const float = v3 + val1Arr[1]
+
+    //   return float ? `${v2 || '0'}.${float}` : v2
+    // })
+
+    // return this
   }
 
   /** 相加 */
@@ -108,6 +148,6 @@ export class Calculator {
   // }
 
   result () {
-    return this.val1Arr.join('.')
+    return this.num1Arr.join('.')
   }
 }
--- test/calculator/multiply.test.ts
+++ test/calculator/multiply.test.ts
@@ -1,125 +1,105 @@
-import { Calculator } from "../../src/Calculator";
-
+import { Calculator } from '../../src/Calculator'
 
 describe('int x int ', () => {
   it('2 x 2 = 4', () => {
-    expect(new Calculator('2').multiply('2').result()).toBe('4');
-  });
-
+    expect(new Calculator('2').multiply('2').result()).toBe('4')
+  })
 
   it('2 x 12 = 24', () => {
-    expect(new Calculator('2').multiply('12').result()).toBe('24');
-  });
-
+    expect(new Calculator('2').multiply('12').result()).toBe('24')
+  })
 
   it('2 x 123 = 246', () => {
-    expect(new Calculator('2').multiply('123').result()).toBe('246');
-  });
+    expect(new Calculator('2').multiply('123').result()).toBe('246')
+  })
 
   it('12 x 12 = 4', () => {
-    expect(new Calculator('12').multiply('12').result()).toBe('144');
-  });
-
+    expect(new Calculator('12').multiply('12').result()).toBe('144')
+  })
 
   it('123 x 123 = 15129', () => {
-    expect(new Calculator('123').multiply('123').result()).toBe('15129');
-  });
+    expect(new Calculator('123').multiply('123').result()).toBe('15129')
+  })
 
   it('1234 x 1234 = 1522756', () => {
-    expect(new Calculator('1234').multiply('1234').result()).toBe('1522756');
-  });
-
+    expect(new Calculator('1234').multiply('1234').result()).toBe('1522756')
+  })
 })
-
 
 describe('int x float ', () => {
   it('2 x 0.2 = 0.4', () => {
-    expect(new Calculator('2').multiply('0.2').result()).toBe('0.4');
-  });
-
+    expect(new Calculator('2').multiply('0.2').result()).toBe(.4')
+  })
 
   it('12 x 0.02 = 24', () => {
-    expect(new Calculator('12').multiply('0.02').result()).toBe('0.24');
-  });
-
+    expect(new Calculator('12').multiply('0.02').result()).toBe('0.24')
+  })
 
   it('123 x 0.008 = 0.984', () => {
-    expect(new Calculator('123').multiply('0.008').result()).toBe('0.984');
-  });
+    expect(new Calculator('123').multiply('0.008').result()).toBe('0.984')
+  })
 
   it('1234 x 0.0005 = 0.617', () => {
-    expect(new Calculator('1234').multiply('0.0005').result()).toBe('0.617');
-  });
-
+    expect(new Calculator('1234').multiply('0.0005').result()).toBe('0.617')
+  })
 
   it('12345 x 0.01006 = 124.1907', () => {
-    expect(new Calculator('12345').multiply('0.01006').result()).toBe('124.1907');
-  });
+    expect(new Calculator('12345').multiply('0.01006').result()).toBe('124.1907')
+  })
 
   it('123456 x 0.12345678 = 15241.48023168', () => {
-    expect(new Calculator('123456').multiply('0.12345678').result()).toBe('15241.48023168');
-  });
-
+    expect(new Calculator('123456').multiply('0.12345678').result()).toBe('15241.48023168')
+  })
 })
-
 
 describe('float x int ', () => {
   it('0.2 x 2 = 0.4', () => {
-    expect(new Calculator('0.2').multiply('2').result()).toBe('0.4');
-  });
-
+    expect(new Calculator('0.2').multiply('2').result()).toBe('0.4')
+  })
 
   it('0.02 x 12 = 24', () => {
-    expect(new Calculator('0.02').multiply('12').result()).toBe('0.24');
-  });
-
+    expect(new Calculator('0.02').multiply('12').result()).toBe('0.24')
+  })
 
   it('0.008 x 123 = 0.984', () => {
-    expect(new Calculator('0.008').multiply('123').result()).toBe('0.984');
-  });
+    expect(new Calculator('0.008').multiply('123').result()).toBe('0.984')
+  })
 
   it('0.0005 x 1234 = 0.617', () => {
-    expect(new Calculator('0.0005').multiply('1234').result()).toBe('0.617');
-  });
-
+    expect(new Calculator('0.0005').multiply('1234').result()).toBe('0.617')
+  })
 
   it('0.01006 x 12345 = 124.1907', () => {
-    expect(new Calculator('0.01006').multiply('12345').result()).toBe('124.1907');
-  });
+    expect(new Calculator('0.01006').multiply('12345').result()).toBe('124.1907')
+  })
 
   it('0.12345678 x 123456 = 15241.48023168', () => {
-    expect(new Calculator('0.12345678').multiply('123456').result()).toBe('15241.48023168');
-  });
-
+    expect(new Calculator('0.12345678').multiply('123456').result()).toBe('15241.48023168')
+  })
 })
-
 
 describe('float x float ', () => {
   it('0.2 x 0.2 = 0.04', () => {
-    expect(new Calculator('0.2').multiply('0.2').result()).toBe('0.04');
-  });
-
+    expect(new Calculator('0.2').multiply('0.2').result()).toBe('0.04')
+  })
 
   it('0.02 x 0.12 = 0.0024', () => {
-    expect(new Calculator('0.02').multiply('0.12').result()).toBe('0.0024');
-  });
-
+    expect(new Calculator('0.02').multiply('0.12').result()).toBe('0.0024')
+  })
 
   it('0.008 x 0.123 = 0.000984', () => {
-    expect(new Calculator('0.008').multiply('0.123').result()).toBe('0.000984');
-  });
+    expect(new Calculator('0.008').multiply('0.123').result()).toBe('0.000984')
+  })
 
   it('0.0005 x 0.1234 = 0.0000617', () => {
-    expect(new Calculator('0.0005').multiply('0.1234').result()).toBe('0.0000617');
-  });
-
+    expect(new Calculator('0.0005').multiply('0.1234').result()).toBe('0.0000617')
+  })
 
   it('0.01006 x 0.12345 = 0.001241907', () => {
-    expect(new Calculator('0.01006').multiply('0.12345').result()).toBe('0.001241907');
-  });
+    expect(new Calculator('0.01006').multiply('0.12345').result()).toBe('0.001241907')
+  })
 
   it('0.12345678 x 0.123456 = 0.01524148023168', () => {
-    expect(new Calculator('0.12345678').multiply('0.123456').result()).toBe('0.01524148023168');
-  });
-
+    expect(new Calculator('0.12345678').multiply('0.123456').result()).toBe('0.01524148023168')
+  })
 })
